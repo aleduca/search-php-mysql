36f4d74993a24fb9db3dd4f6862e34b454f43251
Alexandre Cardoso <xandecar@hotmail.com>
2022-12-21 15:14:28 -0300

    First Commit

---
+++ app/database/models/Book.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\database\models;
+
+use PDO;
+
+class Book
+{
+  public function getBooks(PDO $connection)
+  {
+    try {
+      $prepare = $connection->prepare("select id, title, description from books limit 50");
+      $prepare->execute();
+      return $prepare->fetchAll();
+    } catch (\Throwable $th) {
+      var_dump($th->getMessage());
+    }
+  }
+
+  public function searchBooks(PDO $connection, $search)
+  {
+    try {
+      $prepare = $connection->prepare("select id, title, description from books where title like :search or description like :search");
+      $prepare->execute(['search' => '%' . $search . '%']);
+      return $prepare->fetchAll();
+    } catch (\Throwable $th) {
+      var_dump($th->getMessage());
+    }
+  }
+}
---
+++ public/index.php
@@ -0,0 +1,41 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use app\database\Connection;
+use app\database\models\Book;
+
+$book = new Book;
+$books = $book->getBooks(Connection::getConnection());
+
+if (isset($_GET['s'])) {
+  $search = strip_tags($_GET['s']);
+  $books = $book->searchBooks(Connection::getConnection(), $search);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Search with php and mysql</title>
+</head>
+
+<body>
+  <form method="get">
+    <input type="text" placeholder="Search book" name="s">
+    <button type="submit">Search</button>
+  </form>
+
+  <ul>
+    <?php foreach ($books as $book) : ?>
+      <li><?php echo $book->title; ?></li>
+    <?php endforeach; ?>
+  </ul>
+
+</body>
+
+</html>
